52b3974d0b615898e91c76b3f3858984d42e9945
Alexander Grygoriev <truedevelop@yahoo.com>
2022-05-17 15:43:42 +0300

    half of tabs done

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,23 @@
 
 document.addEventListener('DOMContentLoaded',()=>{
   
-  console.log(' JS script WORKING!');
+  const slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+  document.querySelector('.prev').addEventListener('click',()=>{
+    slider.goTo('prev');
+  });
+  document.querySelector('.next').addEventListener('click',()=>{
+    slider.goTo('next');
+  });
+
+
+
+
 
 });
