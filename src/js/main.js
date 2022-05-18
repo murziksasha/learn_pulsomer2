8b896f164041092a4085b700762f3816046756e1
Alexander Grygoriev <truedevelop@yahoo.com>
2022-05-18 13:01:47 +0300

    tabs DONE

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,46 @@ document.addEventListener('DOMContentLoaded',()=>{
   });
 
 
+  $(function() {
+      
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.wrapper').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    
+  });
 
+  //переход между страницами табов (которая скрыта и обратно)
+  // $('.catalog-item__link').each(function(i){
+  //   $(this).on('click', function(e){
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //     console.log(`i = ${i}`);
+  //     console.log(`e = ${e}`);
+  //   })
+  // })
 
+  // $('.catalog-item__link_back').each(function(i){
+  //   $(this).on('click', function(e){
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //   })
+  // })
 
+  //Оптимизированный вариант совмещающий эти две функции
+  const toggleSlide = (item)=>{
+    $(item).each(function(i){
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    })
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__link_back');
 });
